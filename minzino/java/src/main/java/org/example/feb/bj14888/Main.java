63cddca9510284cb7e758ad65545118992518ffc
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-02-22 16:03:09 +0900

    :sparkles: 연산자 끼워 넣기 - 실패

---
+++ minzino/java/src/main/java/org/example/feb/bj14888/Main.java
@@ -0,0 +1,32 @@
+package main.java.org.example.feb.bj14888;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] operator = new int[4];
+    public static int[] number;
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        number = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+
+//        dfs(?);
+    }
+}
